2b547097cb701aa12ada076f804df0fb2baf2101
Richard McDaniel <richard.lee.mcdaniel@gmail.com>
2025-02-25 05:24:21 +0000

    Upgrade to Laravel 12

--- app/Console/Commands/Init.php
+++ app/Console/Commands/Init.php
@@ -31,10 +31,13 @@ class Init extends Command
         Artisan::call('key:generate');
 
         $this->info('Setting ASSET_URL...');
-        Artisan::call('app:codespaces-asset-url');
+        $this->setAssetUrl();
 
         $this->info('Running migrations...');
         Artisan::call('migrate');
+
+        $this->info('Installing npm dependencies...');
+        Process::run('npm install');
 
         $this->info('Installing Playwright components...');
 
@@ -66,5 +69,49 @@ class Init extends Command
         $this->newLine();
 
         $this->info('Done!');
+    }
+
+    /**
+     * Set ASSET_URL in env based on codespace name.
+     */
+    protected function setAssetUrl()
+    {
+        $codespaceName = env('CODESPACE_NAME');
+        $portDomain = env('GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN');
+
+        if (!$codespaceName || !$portDomain) {
+            $this->error('Missing required GitHub Codespaces environment variables.');
+            return;
+        }
+
+        $assetUrl = "https://{$codespaceName}-80.{$portDomain}";
+
+        if ($this->setEnvVariable('ASSET_URL', $assetUrl)) {
+            $this->info('ASSET_URL set successfully in .env file.');
+        }
+    }
+
+    /**
+     * Set a given key-value pair in the .env file.
+     *
+     * @param string $key
+     * @param string $value
+     * @return bool
+     */
+    protected function setEnvVariable($key, $value)
+    {
+        $envFile = $this->laravel->environmentFilePath();
+        $envContents = file_get_contents($envFile);
+        $pattern = "/^{$key}=.*/m";
+        $replacement = "{$key}={$value}";
+
+        if (preg_match($pattern, $envContents)) {
+            $envContents = preg_replace($pattern, $replacement, $envContents);
+        } else {
+            $envContents .= "\n{$replacement}";
+        }
+
+        file_put_contents($envFile, $envContents);
+        return true;
     }
 }
